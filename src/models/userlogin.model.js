7f0a500ba6d84d7ec1e2b6abeb4bc7c611920f1f
KingAbhi6848 <sabhi6848@gmail.com>
2024-05-23 21:56:11 +0530

    second commit

--- src/models/userlogin.model.js
+++ src/models/userlogin.model.js
@@ -1,69 +1,74 @@
-export default class UserLogin{
-  
-    constructor({firstName,lastName,email,phone,password}){
-        this.firstName = firstName,
-        this.lastName = lastName,
-        this.email = email,
-        this.phone = phone,
+export default class UserLogin {
+
+    constructor({ id, firstName, lastName, email, phone, password }) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
         this.password = password;
     }
 
-    static addJobSeeker(userDetails){
-        const newJobSeeker = new UserLogin(userDetails);
-        jobSeeker.push(newJobSeeker);
-        return {success:true ,newJobSeeker };
+    static addJobSeeker(userDetails) {
+        const id = UserLogin.generateId();
+        const newJobSeeker = new UserLogin({ id, ...userDetails });
+        jobSeekers.push(newJobSeeker);
+        return { success: true, newJobSeeker };
     }
 
-    static getJobSeeker(){
-        return jobSeeker;
+    static getJobSeekers() {
+        return jobSeekers;
     }
 
-    static jobLoginAuth(details){
-       for(let i=0; i<jobSeeker.length ; i++){
-        if(details.email == jobSeeker[i].email && details.password == jobSeeker[i].password){
-            return {success:true , message: 'User Logged in Successfully'};
-        }
-       
-       }
-       return  {success:false , message: 'Invalid Details'};
-    }
-
-
-    static recuriterSignup(details){
-        const newRecuriter = new UserLogin(details);
-        recuriter.push(newRecuriter);
-        return recuriter;
-    }
-
-    static recuriterLogin(details){
-        for(let i =0; i<recuriter.length;i++){
-            if(details.email == recuriter[i].email && details.password == recuriter[i].password){
-                return {success:true, message:"Recuriter Logged In Successfully"}
+    static jobLoginAuth(details) {
+        for (let i = 0; i < jobSeekers.length; i++) {
+            if (details.email === jobSeekers[i].email && details.password === jobSeekers[i].password) {
+                return { success: true, message: 'User Logged in Successfully' };
             }
         }
-        return {success:false,message:"Recuriter Failed to Login"};
+        return { success: false, message: 'Invalid Details' };
     }
 
-    static getRecuriter(){
-        return recuriter;
+    static recruiterSignup(details) {
+        const id = UserLogin.generateId();
+        const newRecruiter = new UserLogin({ id, ...details });
+        recruiters.push(newRecruiter);
+        return { success: true, newRecruiter };
     }
 
-  }
-  
+    static recruiterLogin(details) {
+        for (let i = 0; i < recruiters.length; i++) {
+            if (details.email === recruiters[i].email && details.password === recruiters[i].password) {
+                return { success: true, message: 'Recruiter Logged In Successfully' };
+            }
+        }
+        return { success: false, message: 'Recruiter Failed to Login' };
+    }
 
+    static getRecruiters() {
+        return recruiters;
+    }
 
-const jobSeeker = [{
-    firstName:'abhi',
-    lastName:'sharma',
-    phone:524752144,
-    email:'as684844@gmail.com',
-    password:'1'
+    static generateId() {
+        return Date.now().toString();
+    }
+}
+
+// Mock data for job seekers and recruiters
+const jobSeekers = [{
+    id: Date.now().toString(),
+    firstName: 'abhi',
+    lastName: 'sharma',
+    phone: 524752144,
+    email: 'as684844@gmail.com',
+    password: '1'
 }];
-const jobSeekerApply=[];
-const recuriter =[{
-    firstName:'keshav',
-    lastName:'Maharaj',
-    phone:524755744,
-    email:'keshav@gmail.com',
-    password:'1'
+
+const recruiters = [{
+    id: Date.now().toString(),
+    firstName: 'keshav',
+    lastName: 'Maharaj',
+    phone: 524755744,
+    email: 'keshav@gmail.com',
+    password: '1'
 }];
